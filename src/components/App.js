a8724d112fab5e5f1fff2c19e83ea78db919d830
Elizaveta <elizaveta@ubuntu.ubuntu-domain>
2019-12-09 21:26:48 +0200

    email,mobile and prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
-import React from "react";
-import countries from "../data/countries";
-import cities from "../data/cities";
-import Field from "./Field"
+import React from 'react'
+import countries from '../data/countries'
+import cities from '../data/cities'
+import Field from './Field'
 
 export default class App extends React.Component {
-
   constructor() {
     super()
 
@@ -12,88 +11,101 @@ export default class App extends React.Component {
       username: '',
       password: '',
       repeatPassword: '',
-      country: "1",
-      city: {},
-      gender: "",
+      country: '1',
+      city: '1',
+      gender: '',
+      email: '',
+      mobile: '',
       agree: false,
-      avatar: "",
+      avatar: '',
       errors: {},
-      age: "16"
+      age: '16',
     }
   }
 
   onSubmit = event => {
-    event.preventDefault();
+    event.preventDefault()
     const errors = {}
     if (this.state.username.length < 5) {
-      errors.username = "Reqiured. Must be 5 characters or more"
+      errors.username = 'Reqiured. Must be 5 characters or more'
     }
 
     if (this.state.password.length < 4) {
-      errors.password = "Reqiured. Must be 4 characters or more"
+      errors.password = 'Reqiured. Must be 4 characters or more'
     }
 
     if (this.state.repeatPassword !== this.state.password) {
-      errors.repeatPassword = "Must be equal to password"
+      errors.repeatPassword = 'Must be equal to password'
     }
 
-    if (this.state.age < 18 ) {
-      errors.age = "You must be older 18"
+    if (
+      !this.state.email.includes('@' && '.', 3) ||
+      this.state.email.trim().length < 5
+      // this.state.email.indexOf("@") =< 3 &&
+      // this.state.email.indexOf(".") >= -3
+    ) {
+      console.log('this.state.email', this.state.email, 'invalid emeail')
+      errors.email = 'Invalid email address'
+    }
+
+    if (this.state.mobile.length !== 10) {
+      console.log('this.state.mobile', this.state.mobile)
+      errors.mobile = 'Invalid mobile'
+    }
+
+    if (this.state.age < 18) {
+      errors.age = 'You must be older 18'
     }
 
     if (Object.keys(errors).length > 0) {
       this.setState({
-        errors: errors
+        errors: errors,
       })
-      console.log("errors", this.state.errors)
+      console.log('errors', this.state.errors)
     } else {
       this.setState({
-        errors: {}
+        errors: {},
       })
-      console.log("submit",this.state)
+      console.log('submit', this.state)
     }
   }
 
   onChange = event => {
-    console.log("event target name", event.target.value)
-    console.log("state country city", this.state.country, this.state.city)
+    console.log('event target name', event.target.value)
+    console.log('state country city', this.state.country, this.state.city)
     this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
+      [event.target.name]: event.target.value,
+    })
+  }
 
   onChangeAgree = event => {
-    console.log("event target checked",  event.target.checked)
+    console.log('event target checked', event.target.checked)
     this.setState({
-      [event.target.name]: event.target.checked
-    });
-  };
+      [event.target.name]: event.target.checked,
+    })
+  }
 
   onChangeAvatar = event => {
     const reader = new FileReader()
     reader.onload = event => {
       this.setState({
-        avatar: event.target.result
-      });
-    };
+        avatar: event.target.result,
+      })
+    }
     reader.readAsDataURL(event.target.files[0])
     console.log(event.target.files[0])
-  };
+  }
 
-  getOptionItems = (Items) => Items.map(item =>
-    <option key={item.id} value={item.id}>
-      {item.name}
-    </option>
-  )
+  getOptionItems = Items =>
+    Items.map(item => (
+      <option key={item.id} value={item.id}>
+        {item.name}
+      </option>
+    ))
 
   render() {
-    // console.log(this)
+    console.log(this)
 
-    // const getOptionCountries = countries.map(country => {
-    //   return (<option key={country.id} value={country.id}>
-    //     {country.name}
-    //   </option>)
-    // })
     // console.log(getOptionCountries)
     return (
       <div className="form-container card">
@@ -106,7 +118,7 @@ export default class App extends React.Component {
             name="username"
             value={this.state.username}
             onChange={this.onChange}
-            error = {this.state.errors.username}
+            error={this.state.errors.username}
           />
 
           <Field
@@ -117,7 +129,7 @@ export default class App extends React.Component {
             name="password"
             value={this.state.password}
             onChange={this.onChange}
-            error = {this.state.errors.password}
+            error={this.state.errors.password}
           />
 
           <Field
@@ -128,46 +140,74 @@ export default class App extends React.Component {
             name="repeatPassword"
             value={this.state.repeatPassword}
             onChange={this.onChange}
-            error = {this.state.errors.repeatPassword}
+            error={this.state.errors.repeatPassword}
           />
 
           <div>Gender</div>
-            <div className="form-check-inline">
-              <label className="form-check-label">
-                <input type="radio"
-                  className="form-check-input"
-                  name="gender"
-                  id="female"
-                  value="female"
-                  checked={this.state.gender === "female" ? "active" : ""}
-                  onChange={this.onChange}
-                />Female
-              </label>
-            </div>
-            <div className="form-check-inline">
+          <div className="form-check-inline">
             <label className="form-check-label">
-              <input type="radio"
+              <input
+                type="radio"
                 className="form-check-input"
                 name="gender"
-                id="male"
-                value="male"
-                checked={this.state.gender === "male" ? "active" : ""}
+                id="female"
+                value="female"
+                checked={this.state.gender === 'female' ? 'active' : ''}
                 onChange={this.onChange}
-              />Male
+              />
+              Female
             </label>
           </div>
           <div className="form-check-inline">
             <label className="form-check-label">
-              <input type="radio"
+              <input
+                type="radio"
+                className="form-check-input"
+                name="gender"
+                id="male"
+                value="male"
+                checked={this.state.gender === 'male' ? 'active' : ''}
+                onChange={this.onChange}
+              />
+              Male
+            </label>
+          </div>
+          <div className="form-check-inline">
+            <label className="form-check-label">
+              <input
+                type="radio"
                 className="form-check-input"
                 name="gender"
                 id="not binary"
                 value="not binary"
-                checked={this.state.gender === "not binary" ? "active" : ""}
+                checked={this.state.gender === 'not binary' ? 'active' : ''}
                 onChange={this.onChange}
-              />Not binary
+              />
+              Not binary
             </label>
           </div>
+
+          <Field
+            labelText="Email"
+            id="email"
+            type="text"
+            placeholder="Enter email"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+            error={this.state.errors.email}
+          />
+
+          <Field
+            labelText="Mobile number"
+            id="mobile"
+            type="number"
+            placeholder="Mobile"
+            name="mobile"
+            value={this.state.mobile}
+            onChange={this.onChange}
+            error={this.state.errors.mobile}
+          />
 
           <div className="form-group">
             <label htmlFor="country">Country</label>
@@ -192,31 +232,39 @@ export default class App extends React.Component {
               name="city"
               onChange={this.onChange}
             >
-              {this.getOptionItems(Object.values(cities).filter(key => key["country"] === [this.state.country] ))}
+              {Object.values(cities)
+                .filter(key => key['country'] === 2)
+                .map(city => (
+                  <option key={city.id} value={city.id}>
+                    {city.name}
+                  </option>
+                ))}
 
-              {/* { Object.values(cities).map(cities =>
-                <option key={this.state.country} value={cities.id}>
-                {cities.name}
-                </option>
-              )} */}
+              {/* {this.getOptionItems(Object.values(cities).filter(key => key["country"] === [this.state.country] ))} */}
             </select>
           </div>
 
           <div className="form-group">
             <label htmlFor="formControlRange">Age</label>
-              <input type="number"
-                className="form-control-range"
-                id="formControlRange"
-                name="age"
-                value={this.state.age}
-                onChange={this.onChange}
-              />
-            { this.state.errors.age && (<div className="invalid-feedback"> {this.state.errors.age} </div>) }
+            <input
+              type="number"
+              className="form-control-range"
+              id="formControlRange"
+              name="age"
+              value={this.state.age}
+              onChange={this.onChange}
+            />
+            {this.state.errors.age && (
+              <div className="invalid-feedback"> {this.state.errors.age} </div>
+            )}
           </div>
 
           <div className="form-group">
-            <label className="form-group-label" htmlFor="customFileLang">Upload your avatar</label>
-            <input type="file"
+            <label className="form-group-label" htmlFor="customFileLang">
+              Upload your avatar
+            </label>
+            <input
+              type="file"
               className="form-group-input"
               id="avatar"
               name="avatar"
@@ -226,18 +274,21 @@ export default class App extends React.Component {
 
           <div className="form-check">
             <label className="form-check-label">
-              <input type="checkbox"
+              <input
+                type="checkbox"
                 className="form-check-input"
                 value="true"
                 id="agree"
                 name="agree"
                 checked={this.state.agree}
                 onChange={this.onChangeAgree}
-              />I agree to process the data
+              />
+              I agree to process the data
             </label>
           </div>
 
-          <button type="submit"
+          <button
+            type="submit"
             className="btn btn-primary w-100"
             onClick={this.onSubmit}
           >
@@ -245,6 +296,6 @@ export default class App extends React.Component {
           </button>
         </form>
       </div>
-    );
+    )
   }
 }
